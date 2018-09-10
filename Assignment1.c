0817fa804a17619938de82b6d800cfd8085ae4fc
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-09-10 10:40:07 -0500

    Testing switch, rand, and function functionality

--- Assignment1.c
+++ Assignment1.c
@@ -1,7 +1,46 @@
-#include <stdio.h>
 
-int main() {
-    printf("Hello World\n");
+
+char operator(void);
+
+int main(void) {
+    int questions;
+
+    srand(time(NULL));
+
+    printf("How many questions would you like to answer? ");
+    scanf("%d", &questions);
+    printf("The response submitted is: %d\n", questions);
+
+    char Operator1 = operator();
+    printf("Operator returned from function is: %c", Operator1);
 
     return 0;
+}
+
+char operator(void) {
+    int randOperatorNum = rand() % 4 + 1;
+    char randOperator = 0;
+    printf("Random operator nummber generated is: %d\n", randOperatorNum);
+
+    switch (randOperatorNum) {
+        case 1:
+            randOperator = '+';
+            break;
+        case 2:
+            randOperator = '-';
+            break;
+        case 3:
+            randOperator = '*';
+            break;
+        case 4:
+            randOperator = '/';
+            break;
+        default:
+            "ERROR: Invalid number.";
+
+    }
+    printf("Random Operator within operator() function: %c\n", randOperator);
+    return randOperator;
+
+
 }

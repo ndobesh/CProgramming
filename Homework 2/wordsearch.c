22f1ee800b01a544d7aed189116edf69da5b77d4
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-09-25 18:06:13 -0500

    Will be inputting wordsearch file as command line argument. Added code to input file.

--- Homework 2/wordsearch.c
+++ Homework 2/wordsearch.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-    printf("I was able to compile! Yay!\n");
+    FILE *myFile = fopen(argv[1], "r");
+    if (myFile == NULL) {
+        fprintf(stderr, "Failed to open file\n");
+        exit(1);
+    }
+
+    //Input Validation
+    assert(argc > 1);
+
+    //DEBUG Statement
+    printf("This will print if assertion is true");
     return 0;
 }
 

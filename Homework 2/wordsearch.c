fa921da712c0ec29e63e4a6c1711300c328abe61
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-09-29 12:19:50 -0500

    Added functionality to check size of first line from file. This will check to see how big the 2D array will have to be.

--- Homework 2/wordsearch.c
+++ Homework 2/wordsearch.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <memory.h>
 
+#define MAXENTRIES 50
+
+
+//TODO: Create array of whatever size data1/2/3 wordsearch is
+//TODO: Take out spaces of inputted 2D array so that I can process it
+//TODO: Store words after 2D array in file to be compared against
+//TODO: Output results
 int main(int argc, char *argv[]) {
 
     FILE *myFile = fopen(argv[1], "r");
@@ -14,7 +22,25 @@ int main(int argc, char *argv[]) {
     assert(argc > 1);
 
     //DEBUG Statement
-    printf("This will print if assertion is true");
+    /*printf("This will print if assertion is true");*/
+
+    char firstLine[MAXENTRIES];
+
+    fgets(firstLine, MAXENTRIES, myFile);
+
+    //DEBUG: Used to see what is put in firstLine by fgets function.
+    printf("%s\n", firstLine);
+
+    //This will be used to create 2D array size later
+    size_t numberofElementsWithSpaces = strlen(firstLine);
+
+    //DEBUG Statement
+    /*printf("Length of first line is: %zu\n", numberofElementsWithSpaces);*/
+
+    char ArrayWithoutSpaces[(numberofElementsWithSpaces - 1) / 2][(numberofElementsWithSpaces - 1) / 2];
+
+
+    fclose(myFile);
     return 0;
 }
 

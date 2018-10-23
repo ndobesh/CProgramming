64a05c5162f61cc37354e22b653d1eb69f966013
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-10-23 13:27:44 -0500

    Assigned values to struct memebers in initialize function. Started to open files that could be used as a command line argument. Included more header files needed for later.

--- Homework 3/VirtualComp.c
+++ Homework 3/VirtualComp.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <memory.h>
 
 
-//TODO: Put in comments for each variable in structs
 struct registers {
-    int accumalator;
-    int InstructionCounter;
-    int InstructionRegister;
-    int OperationCode;
-    int Operand;
-    int memory[100];
+    int accumalator; //register that operations are performed upon.
+    int InstructionCounter; //register that stores the address of the current instruction to be executed
+    int InstructionRegister; //register that stores the current instruction fetched from VM memory.
+    int OperationCode; // register that stores the opeation to be done (i.e. READ, BRAN, ADD, etc.)
+    int Operand; // register that stores the memory address to be used in instruction.
+    int memory[100]; // Array used as memory of VM wich each location having 4 digit integers.
 };
 
+//Next 4 structs contain operationCode values.
 struct inputOutput {
-    const int read = 10;
-    const int writ = 11;
-    const int prnt = 12;
+    const int READ;
+    const int WRIT;
+    const int PRNT;
 };
 
 struct loadStore {
-    const int load = 20;
-    const int stor = 21;
-    const int set = 22;
+    const int LOAD;
+    const int STOR;
+    const int SET;
 };
 
 struct arithmetic {
-    const int add = 30;
-    const int sub = 31;
-    const int div = 32;
-    const int mult = 33;
-    const int mod = 34;
+    const int ADD;
+    const int SUB;
+    const int DIV;
+    const int MULT;
+    const int MOD;
 };
 
 struct control {
-    const int bran = 40;
-    const int brng = 41;
-    const int brzr = 42;
-    const int halt = 99;
+    const int BRAN;
+    const int BRNG;
+    const int BRZR;
+    const int HALT;
 };
 
 
@@ -45,10 +48,10 @@ void execute(void);
 
 void printMemory(void);
 
-void initialize(void);
+void initialize(int, char *);
 
-int main(void) {
-    initialize();
+int main(int argc, char *argv[]) {
+    initialize(argc, argv);
     compile();
     execute();
     return 0;
@@ -61,7 +64,7 @@ void compile(void) {
 }
 
 //TODO: change  void if necessary
-//Flesh out
+//TODO: Flesh out
 void execute(void) {
     printMemory();
 }
@@ -75,7 +78,29 @@ void printMemory(void) {
 //TODO: change  void if necessary
 //TODO: Flesh out
 //Opens files
-void initialize(void) {
+void initialize(int argc, char *argv) {
+    //Assigning values to struct members.
+    struct registers structregisters = {0, 0, 0, 0, 0, 0, 0};
+    struct inputOutput structinputOutput = {10, 11, 12};
+    struct loadStore structloadStore = {20, 21, 22};
+    struct arithmetic structarithmetic = {30, 31, 32, 33, 34};
+    struct control structcontrol = {40, 41, 42, 99};
+
+    //Open each file to be used in program with "read" permission
+    assert(argc > 1);
+    if (0 == strcmp(&argv[1], "prog1")) {
+        FILE *prog1 = fopen(&argv[1], "r");
+        if (prog1 == NULL) {
+            fprintf(stderr, "Failed to open file\n");
+            exit(1);
+        } else if (0 == strcmp(&argv[1], "prog2")) {
+            FILE *prog2 = fopen(&argv[1], "r");
+            if (prog2 == NULL) {
+                fprintf(stderr, "Failed to open file\n");
+                exit(1);
+            }
+        }
+    }
 
 }
 

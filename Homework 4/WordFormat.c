0b4cf358cbac48736df54f974f57a7392cf22e80
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-11-04 21:33:45 -0600

    Initial setup

--- Homework 4/WordFormat.c
+++ Homework 4/WordFormat.c
@@ -1,2 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void initialize(void);
 
+void closure(void);
+
+int main(int argc, char *argv[]) {
+    char *charPerLinePointer = argv[1];
+    int charPerLine = atoi(charPerLinePointer);
+
+    printf("%d", charPerLine);
+
+    initialize();
+    closure();
+    return 0;
+}
+
+void initialize(void) {
+
+}
+
+void closure(void) {
+
+}
